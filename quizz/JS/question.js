f5d4d7d49e7ad90ac0f0a103dde7200637bb7a87
Lois419 <lois.ijioma@techskool.org>
2025-07-14 16:42:35 +0300

    ANEW
    ANEW

---
+++ quizz/JS/question.js
@@ -0,0 +1,51 @@
+const testData = {
+  questions: [
+    {
+      id: "q1",
+      text: "What is 2 + 2?",
+      options: ["3", "4", "5", "6"],
+      answer: "4",
+    },
+    {
+      id: "q2",
+      text: "Capital of France?",
+      options: ["London", "Berlin", "Paris", "Rome"],
+      answer: "Paris",
+    },
+    {
+      id: "q3",
+      text: "5 x 5 = ?",
+      options: ["10", "20", "25", "30"],
+      answer: "25",
+    },
+    {
+      id: "q4",
+      text: "Color of the sky?",
+      options: ["Red", "Green", "Blue", "Yellow"],
+      answer: "Blue",
+    },
+    {
+      id: "q5",
+      text: 'Opposite of "cold"?',
+      options: ["Cool", "Warm", "Hot", "Ice"],
+      answer: "Hot",
+    },
+  ],
+};
+
+const jss1MathData = {
+  questions : [
+    {
+      id: "q1",
+      text:" 10 *10 ",
+      options : ["20, 50, 80, 100"],
+      answer:"100",
+    },
+      {
+      id: "q2",
+      text:" 10 + 10",
+      options : ["20, 50, 80, 100"],
+      answer:"20",
+    },
+  ]
+}
