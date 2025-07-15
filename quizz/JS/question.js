2af8459b055b43794bf69d98c27a8a70f9ad5377
Lois419 <lois.ijioma@techskool.org>
2025-07-15 16:22:12 +0300

    commit
    c

--- quizz/JS/question.js
+++ quizz/JS/question.js
@@ -34,18 +34,123 @@ const testData = {
 };
 
 const jss1MathData = {
-  questions : [
+  questions: [
     {
       id: "q1",
-      text:" 10 *10 ",
-      options : ["20, 50, 80, 100"],
-      answer:"100",
+      text: " What's 150 * 70 / 10 ",
+      options: ["50", "100", "1050"],
+      answer: "1050",
     },
-      {
+    {
       id: "q2",
-      text:" 10 + 10",
-      options : ["20, 50, 80, 100"],
-      answer:"20",
+      text: " What's 150 + 70 + 30",
+      options: ["150", "50", "110"],
+      answer: "110",
     },
-  ]
-}
+    {
+      id: "q3",
+      text: " What's the L.C.M of 4 and 6",
+      options: ["12", "6", "24"],
+      answer: "12",
+    },
+    {
+      id: "q4",
+      text: " Simplify 4/8",
+      options: ["1/2", "1/4", "1/8"],
+      answer: "1/2",
+    },
+    {
+      id: "q5",
+      text: "  What is a 1010 in base 10",
+      options: ["8", "10", "12"],
+      answer: "10",
+    },
+  ],
+};
+
+const jss2MathData = {
+  questions: [
+    {
+      id: "q1",
+      text: " Approximate 550 to the nearest hundred ",
+      options: ["500", "555", "600"],
+      answer: "1050",
+    },
+    {
+      id: "q2",
+      text: " What is the mean of these numbers (1,2,3,4,5)",
+      options: ["1", "3", "5"],
+      answer: "3",
+    },
+    {
+      id: "q3",
+      text: " What is the mode of these numbers (1,1,3,4,5)",
+      options: ["1", "3", "5"],
+      answer: "1",
+    },
+    {
+      id: "q4",
+      text: " What is the range of these numbers (1,1,3,4,5)",
+      options: ["1", "4", "5"],
+      answer: "4",
+    },
+    {
+      id: "q5",
+      text: "  2/3 +  1/3",
+      options: ["3/3", "2/2", "1/2"],
+      answer: "3/3",
+    },
+  ],
+};
+
+const jss3MathData = {
+  questions: [
+    {
+      id: "q1",
+      text: " What's 150 * 70 / 10 ",
+      options: ["50", "100", "1050"],
+      answer: "1050",
+    },
+    {
+      id: "q2",
+      text: " What's 150 + 70 + 30",
+      options: ["150", "50", "110"],
+      answer: "110",
+    },
+    {
+      id: "q3",
+      text: " What's the L.C.M of 4 and 6",
+      options: ["12", "6", "24"],
+      answer: "12",
+    },
+    {
+      id: "q4",
+      text: " Simplify 4/8",
+      options: ["1/2", "1/4", "1/8"],
+      answer: "1/2",
+    },
+    {
+      id: "q5",
+      text: "  What is a 1010 in base 10",
+      options: ["8", "10", "12"],
+      answer: "10",
+    },
+  ],
+};
+
+const jss1EtiquetteData = {
+  questions: [
+    {
+      id: "q1",
+      text: " 10 *10 ",
+      options: ["20", "50", "80", "100"],
+      answer: "100",
+    },
+    {
+      id: "q2",
+      text: " 10 + 10",
+      options: ["20", "50", "80", "100"],
+      answer: "20",
+    },
+  ],
+};
